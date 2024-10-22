a1509b5aace959163812c4e09ee57b06de2a4908
fajartxtw <fajarbaharuddin9@gmail.com>
2024-10-22 12:49:52 +0700

    Add files via upload

--- app/Models/News.php
+++ app/Models/News.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class News extends Model
+{
+    protected $table ='newses';
+
+    function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    function reporter(): BelongsTo
+    {
+        return $this->belongsTo(NewsReporter::class, 'reporter_id');
+    }
+}
